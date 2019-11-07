7b1b0f23b48017afe3666b98697c99308ac9929a
jacekgorsk01 <jacekgorski01@gmail.com>
2019-11-07 14:54:11 +0100

    Multitool komentarze

--- homeworks/day3/create_menu.py
+++ homeworks/day3/create_menu.py
@@ -15,15 +15,17 @@ def menu_builder(file_list,line):
     Print string
     '''
     print("*** W I E L O   N A R Z Ę D Ź *** by JACEK VER 1.0\n")
+    #otwieram w pętli pliki z listy
     for i, file in enumerate(file_list):
         with open(file, "r") as file:
-
+            #otwarty plik cztam linia po linii do linii podanej w parametrze funkcji.
+            # Nie chciałem czytać całego pliku i wybierać linii zwłaszcza, że ta była jest w mioch plikach na początku
             for j in range(line):
                 line_content = file.readline()
-            if j == line-1:
+            if j == line-1: #czytam linie aż znajduje wskazaną w zminnej żeby ją wydrukować
                 end = len(line_content) - 9
-                text = (line_content[13:(end)])
-                print(f"({i + 1}) {text}.")
+                text = (line_content[13:(end)]) #odcinam znaki z kodu (od printa)
+                print(f"({i + 1}) {text}.") #i+1 żeby odpowiednio ponumerować menu
                 continue
 
     print("(R) Wylosuj za mnie\n(Q) Wyjdź z WIELO NARZĘDZIA")
--- homeworks/day3/multitool.py
+++ homeworks/day3/multitool.py
@@ -3,9 +3,9 @@ from create_menu import menu_builder
 
 menu_builder(["Z1.py", "Z2.py", "Z3.py", "Z4.py", "Z5.py"], 3)
 
-choice = input("\nWybierz program (1-10) lub: ")
-
-right_values = ["1", "2", "3", "4", "5", "R", "Q"]
+choice = input("\nWybierz program (1-10,"R","Q"): ")
+#sprawdzenie poprawności wyboru
+right_values = ["1", "2", "3", "4", "5", "Q", "R"]
 
 while choice not in right_values:
     print('Nieprawidłowy wybór, wpisz liczbę w przedziale 1-10, "R" lub "Q": ')
@@ -13,7 +13,7 @@ while choice not in right_values:
 
 print(f"Wybrałeś: {choice}")
 if choice == "R":
-        choice = random.choice(right_values[0:6])
+        choice = random.choice(right_values[0:6]) #wykluczam by nie wylosowało się ponownie R
         print(f"Wylosowałęś opcję {choice}")
 else:
     pass
@@ -31,3 +31,5 @@ elif choice == "4":
         import Z4.py
 elif choice == "5":
         import Z5.py
+
+#dorobie kolejne, zabrakło czasu.
